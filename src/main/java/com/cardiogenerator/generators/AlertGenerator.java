33e30db9419313c514e27543f3ef6c83c69679b8
Vlad <vladstef2004@gmail.com>
2024-04-15 22:34:43 +0300

    Line Wrapping changed

--- src/main/java/com/cardiogenerator/generators/AlertGenerator.java
+++ src/main/java/com/cardiogenerator/generators/AlertGenerator.java
@@ -21,22 +21,27 @@ public class AlertGenerator implements PatientDataGenerator {
                 if (randomGenerator.nextDouble() < 0.9) { // 90% chance to resolve
                     alertStates[patientId] = false;
                     // Output the alert
-                    outputStrategy.output(patientId, System.currentTimeMillis(), "Alert", "resolved");
+                    outputStrategy.output(patientId, System.currentTimeMillis(),
+                     "Alert", "resolved");
                 }
             } else {
-                //Lambda was renamed to lambda because local variable names should be written in lowerCamelCase (5.2.7 Google Java Style Guide)
-                double lambda = 0.1; // Average rate (alerts per period), adjust based on desired frequency
+                //Lambda was renamed to lambda because local variable names should be written in lowerCamelCase
+                // (5.2.7 Google Java Style Guide)
+                // Average rate (alerts per period), adjust based on desired frequency
+                double lambda = 0.1; 
                 double p = -Math.expm1(-lambda); // Probability of at least one alert in the period
                 boolean alertTriggered = randomGenerator.nextDouble() < p;
 
                 if (alertTriggered) {
                     alertStates[patientId] = true;
                     // Output the alert
-                    outputStrategy.output(patientId, System.currentTimeMillis(), "Alert", "triggered");
+                    outputStrategy.output(patientId, System.currentTimeMillis(),
+                     "Alert", "triggered");
                 }
             }
         } catch (Exception e) {
-            System.err.println("An error occurred while generating alert data for patient " + patientId);
+            System.err.println("An error occurred while generating alert data for patient "
+             + patientId);
             e.printStackTrace();
         }
     }
