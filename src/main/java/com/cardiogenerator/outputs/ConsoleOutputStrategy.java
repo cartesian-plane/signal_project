22e7d77ea8c9f23dca93a2039e798f5217bcef32
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-04-16 11:40:03 +0200

    Add javadoc for ConsoleOutputStrategy.java

--- src/main/java/com/cardiogenerator/outputs/ConsoleOutputStrategy.java
+++ src/main/java/com/cardiogenerator/outputs/ConsoleOutputStrategy.java
@@ -1,6 +1,8 @@
 package com.cardiogenerator.outputs;
 
+/** Strategy used by the strategy pattern. */
 public class ConsoleOutputStrategy implements OutputStrategy {
+    /** Outputs patient data to the console */
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         System.out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
