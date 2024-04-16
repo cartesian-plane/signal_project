5ecfd1dfc9131f01163eb90c1824488450b6e662
Vlad <vladstef2004@gmail.com>
2024-04-16 16:16:39 +0300

    Style violations fixed

--- src/main/java/com/cardiogenerator/generators/AlertGenerator.java
+++ src/main/java/com/cardiogenerator/generators/AlertGenerator.java
@@ -1,5 +1,10 @@
-package com.cardiogenerator.generators;
+package com.cardiogenerator.generators; // package name contained underscores. It cannot contain
+// underscores (5.2.1).
 
+// The imports were separated by a space, which is wrong. All imports (non-static) 
+// must be placed in a single block with no spaces in-between (3.3.3).
+// Imports' names are not in ASCII sort order. All imports' names within a block
+// must be in ASCII sort order (3.3.3).
 import com.cardiogenerator.outputs.OutputStrategy;
 import java.util.Random;
 
@@ -7,7 +12,8 @@ public class AlertGenerator implements PatientDataGenerator {
 
     public static final Random randomGenerator = new Random();
 
-    // AlertStates was renamed to alertStates because local variable names should be written in lowerCamelCase (5.2.7 Google Java Style Guide)
+    // AlertStates was renamed to alertStates because local variable 
+    // names should be written in lowerCamelCase (5.2.7)
     private boolean[] alertStates; // false = resolved, true = pressed
 
     public AlertGenerator(int patientCount) {
@@ -25,11 +31,13 @@ public class AlertGenerator implements PatientDataGenerator {
                      "Alert", "resolved");
                 }
             } else {
-                // Lambda was renamed to lambda because local variable names should be written in lowerCamelCase
-                // (5.2.7 Google Java Style Guide)
-                // Average rate (alerts per period), adjust based on desired frequency
-                double lambda = 0.1; 
-                double p = -Math.expm1(-lambda); // Probability of at least one alert in the period
+                // Lambda was renamed to lambda because local variable names 
+                // should be written in lowerCamelCase (5.2.7)
+                double lambda = 0.1; // Average rate (alerts per period), adjust 
+                // based on desired frequency
+
+                double p = -Math.expm1(-lambda); // Probability of at least one 
+                // alert in the period
                 boolean alertTriggered = randomGenerator.nextDouble() < p;
 
                 if (alertTriggered) {
