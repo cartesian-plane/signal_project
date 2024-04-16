b5d5f8d07ef33b10df027657f35c6545b0626cd7
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-04-16 11:34:28 +0200

    Add javadoc for PatientDataGenerator.java

--- src/main/java/com/cardiogenerator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardiogenerator/generators/PatientDataGenerator.java
@@ -2,6 +2,10 @@ package com.cardiogenerator.generators;
 
 import com.cardiogenerator.outputs.OutputStrategy;
 
+/** Specifies the methods that must be implemented by all PatientDataGenerators.
+ * If a new data generator is to be added, it must implement this interface.
+ */
 public interface PatientDataGenerator {
+    /** Generate data for a patient, with a given output strategy*/
     void generate(int patientId, OutputStrategy outputStrategy);
 }
