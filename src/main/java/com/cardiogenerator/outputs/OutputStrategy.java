238382af4910a37ba0eb4f51848cabcc7228678f
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-04-16 11:38:13 +0200

    Add javadoc for OutputStrategy.java

--- src/main/java/com/cardiogenerator/outputs/OutputStrategy.java
+++ src/main/java/com/cardiogenerator/outputs/OutputStrategy.java
@@ -1,5 +1,8 @@
 package com.cardiogenerator.outputs;
 
+/** Specifies the methods that must be implemented by every OutputStrategy.
+ * If a new output strategy is to be added, it must implement this interface.
+ * */
 public interface OutputStrategy {
     void output(int patientId, long timestamp, String label, String data);
 }
