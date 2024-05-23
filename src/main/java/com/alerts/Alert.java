74e7f8e65ec2b4ee0f1d1fcca64018831caa87fc
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-05-23 21:48:13 +0200

    Override toString() and equals() in Alert

--- src/main/java/com/alerts/Alert.java
+++ src/main/java/com/alerts/Alert.java
@@ -2,25 +2,42 @@ package com.alerts;
 
 // Represents an alert
 public class Alert {
-    private String patientId;
-    private String condition;
-    private long timestamp;
 
-    public Alert(String patientId, String condition, long timestamp) {
-        this.patientId = patientId;
-        this.condition = condition;
-        this.timestamp = timestamp;
-    }
+  private String patientId;
+  private String condition;
+  private long timestamp;
 
-    public String getPatientId() {
-        return patientId;
-    }
+  public Alert(String patientId, String condition, long timestamp) {
+    this.patientId = patientId;
+    this.condition = condition;
+    this.timestamp = timestamp;
+  }
 
-    public String getCondition() {
-        return condition;
-    }
+  public String getPatientId() {
+    return patientId;
+  }
 
-    public long getTimestamp() {
-        return timestamp;
+  public String getCondition() {
+    return condition;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  // toString method
+  @Override
+  public String toString() {
+    return "Patient ID: " + patientId + ", Condition: " + condition + ", Timestamp: " + timestamp;
+  }
+
+  @Override
+  public boolean equals(Object alert) {
+    if (alert instanceof Alert) {
+      Alert other = (Alert) alert;
+      return this.patientId.equals(other.patientId) && this.condition.equals(other.condition) && this.timestamp == other.timestamp;
+    } else {
+      return false;
     }
+  }
 }
