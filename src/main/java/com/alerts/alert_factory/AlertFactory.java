feb454945f095130f39ac60a9f080f936f0bded2
Vlad <vladstef2004@gmail.com>
2024-06-08 19:45:14 +0200

    alert factory

--- src/main/java/com/alerts/alert_factory/AlertFactory.java
+++ src/main/java/com/alerts/alert_factory/AlertFactory.java
@@ -4,5 +4,5 @@ import com.alerts.Alert;
 
 public abstract class AlertFactory {
     // creating an alert
-    public abstract Alert createAlert(String patientId, String condition, long timestamp);
+    public abstract Alert createAlert(int patientId, String condition, long timestamp);
 }
